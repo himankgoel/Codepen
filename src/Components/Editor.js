efa5c98d37b7882d1cd316b14386303e73d8faf7
himankgoel <himankgoel12@gmail.com>
2020-10-22 13:48:10 +0530

    Added expand and collapse buttons

--- src/Components/Editor.js
+++ src/Components/Editor.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import "codemirror/lib/codemirror.css";
 import "codemirror/theme/material.css";
 import "codemirror/mode/xml/xml"; //HTML 
@@ -8,15 +8,20 @@ import "codemirror/mode/css/css";
 import {Controlled as ControlledEditor} from "react-codemirror2";
 export default function Editor(props) {
 
+    const [open,setOpen] = useState(true);
     const {language , displayName,value,onChange } = props;
+
     function handleChange(editor,data,value){
         onChange(value);
     }
     return (
-        <div className="editor-container">
+        <div className={`editor-container ${open ? '' : 'collapsed'}`}>
             <div className="editor-title">
                 {displayName}
-                <button>O/C</button>
+                <i  className={`fas fa-${open ? 'compress' : 'expand'}-alt`}
+                    onClick={() => setOpen(prevOpen => !prevOpen)}>
+                </i>
+                
             </div>
         <ControlledEditor 
             onBeforeChange={handleChange}
