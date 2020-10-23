bd459ef0fcdd96badfb01944f848835369c3cf9e
himankgoel <himankgoel12@gmail.com>
2020-10-23 17:15:07 +0530

    Updates

---
+++ precache-manifest.78010476917abf112bb9a854517f79e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "850db8a2405e96a7a7664a3f7d7284d0",
+    "url": "/codepen/index.html"
+  },
+  {
+    "revision": "1ca9cf0fa2e978703cf7",
+    "url": "/codepen/static/css/2.ce280647.chunk.css"
+  },
+  {
+    "revision": "1ca9cf0fa2e978703cf7",
+    "url": "/codepen/static/js/2.3dc7cd65.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/codepen/static/js/2.3dc7cd65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a45b0626a916b81276b1",
+    "url": "/codepen/static/js/main.cc3bfe8d.chunk.js"
+  },
+  {
+    "revision": "7934bb05f5c6f04fab84",
+    "url": "/codepen/static/js/runtime-main.332a2d98.js"
+  }
+]);
